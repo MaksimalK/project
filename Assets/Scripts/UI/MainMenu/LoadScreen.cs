9b2852b725230b8c8719739cf4fd5113b68b7cf0
MaksimalK <MaksMK5@mail.ru>
2022-09-22 23:40:14 +0300

    add MaimMenu scene

---
+++ Assets/Scripts/UI/MainMenu/LoadScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+namespace Game.UI.MainMenu
+{
+    public class LoadScreen : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup screen;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            screen.alpha = 1;
+        }
+
+        public void Hide() =>
+            StartCoroutine(ShowRoutine());
+
+        private IEnumerator ShowRoutine()
+        {
+            while (screen.alpha > 0)
+            {
+                screen.alpha -= 0.03f;
+                yield return new WaitForSeconds(0.03f);
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/StartGameButton.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game.UI.MainMenu
+{
+    [RequireComponent(typeof(Button))]
+    public class StartGameButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private LoadScreen loadScreen;
+
+        private void Awake()
+        {
+            button.onClick.AddListener(LoadScene);
+        }
+
+        private void LoadScene()
+        {
+            StartCoroutine(Load("SampleScene"));
+        }
+
+        private IEnumerator Load(String name)
+        {
+            LoadScreen loadScreen1 = Instantiate(loadScreen);
+            GameObject.DontDestroyOnLoad(loadScreen1);
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
+            asyncLoad.allowSceneActivation = false;
+            while (asyncLoad.progress > 0.8)
+            {
+                yield return null;
+            }
+            loadScreen1.Hide();
+            asyncLoad.allowSceneActivation = true;
+
+        }
+    }
+}
