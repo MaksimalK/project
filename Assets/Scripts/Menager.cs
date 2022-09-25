ed8dbf71eef6c3db40a91b366f23f3d02b0213ab
MaksimalK <MaksMK5@mail.ru>
2022-09-25 16:20:58 +0300

    One

---
+++ Assets/Scripts/Menager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menager : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+    public int maxEnemiesOnScreen;
+    public int TotalEnemies;
+    public int enemiesPerSpawn;
+
+    int enemiesOnScreen = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
